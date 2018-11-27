b5baedba2288ad4b7d3aa1b2ebe6bac8b64be174
Jin <jinu0125@gmail.com>
2018-11-27 08:52:34 -0800

    1. a)b) done

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+using namespace std;
 
+template <typename K = string, typename V = int>
+class mymap
+{
+
+
+};
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
